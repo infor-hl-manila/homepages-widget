39b62a46b0276b5bcb93e5e60b98a585b3ac1a85
Eric Angeles <enrico.angeles@infor.com>
2019-07-16 23:31:24 +0800

    remove angularjs context and instance

--- Samples/Widgets/infor.crm.reminders/widget-aot.ts
+++ Samples/Widgets/infor.crm.reminders/widget-aot.ts
@@ -1,8 +1,8 @@
-import { IWidgetContext2, IWidgetInstance2 } from "lime";
+import { IWidgetContext, IWidgetInstance } from "lime";
 import { RemindersWidgetComponent } from "./main";
 import { RemindersWidgetModuleNgFactory } from "./main.ngfactory";
 
-export const widgetFactory = (context: IWidgetContext2): IWidgetInstance2 => {
+export const widgetFactory = (context: IWidgetContext): IWidgetInstance => {
 	return {
 		angularConfig: {
 			moduleFactory: RemindersWidgetModuleNgFactory,
